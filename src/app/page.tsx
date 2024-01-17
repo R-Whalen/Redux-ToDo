506176fb6966fe9fb95400aab213ac73dd45ea54
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2024-01-17 21:54:09 +0000

    setup redux store provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,22 @@
 
 import Header from '@/app/components/Header'
 import { ThemeProvider } from 'next-themes'
-import { Footer, ToDoList } from '@/app/components'
+import { Footer, ToDo } from '@/app/components'
+import store from '../redux/store.js'
+import { Provider } from 'react-redux';
 
 export default function Home() {
   return (
-    <>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+    <Provider store={store}>
+      <ThemeProvider attribute="class" enableSystem>
         <main className='flex flex-col justify-between min-h-screen'>
           <div>
             <Header />
-            <ToDoList />
+            <ToDo />
           </div>
           <Footer />
         </main>
       </ThemeProvider>
-    </>
+    </Provider>
   )
 }

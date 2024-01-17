f092c1629657affa336ecdc16e6ac5fc00b0ec1f
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2024-01-17 19:05:22 +0000

    complete footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,17 @@
 'use-client';
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 const Footer = () => {
+
     return (
-        <footer>
-            Footer
-        </footer>
+        <div className='flex flex-col justify-center items-center'>
+            <hr className='w-full md:w-[80%] text-dark-600' />
+            <footer className='flex justify-center items-center p-8 gap-2'>
+                By Robert Whalen <a href='https://github.com/R-Whalen'><FontAwesomeIcon icon={faGithub} className='h-6 dark:text-white' /></a>
+            </footer>
+        </div>
     )
 }
 

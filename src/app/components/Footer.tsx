e1a2f41ce1557047426da84c22edd978589cf2d9
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2024-01-17 20:14:46 +0000

    Make Header and Footer styling consistent

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
     return (
         <div className='flex flex-col justify-center items-center'>
-            <hr className='w-full md:w-[80%] text-dark-600' />
+            <hr className='w-full md:w-[80%] border-dark-600 dark:border-dark-200' />
             <footer className='flex justify-center items-center p-8 gap-2'>
                 By Robert Whalen <a href='https://github.com/R-Whalen'><FontAwesomeIcon icon={faGithub} className='h-6 dark:text-white' /></a>
             </footer>
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,9 @@ import ToggleTheme from "./ToggleTheme";
 
 const TopBar = () => {
 
-    return <header className='flex justify-end items-center border-b-1 border-b-dark-600 shadow-md h-32 md:h-16 p-8'>
+    return <header className='
+    flex justify-end items-center border-b-1 border-b-dark-600 dark:border-b-dark-200 
+    bg-dark-100 dark:bg-dark-300 shadow-md h-32 md:h-16 p-8'>
         <ToggleTheme />
     </header>
 }
